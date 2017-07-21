4ab738f7bbd2663b56dbc32b6603c4f1c7731e30
Overflocat <wyf2014@mail.ustc.edu.cn>
2017-07-21 09:47:23 +0800

    Prepare for some modification

---
+++ FocusMeasure.m
@@ -0,0 +1,68 @@
+function FocusMeasure(Measure)
+    WINSIZE = 5; %Image.Dimx / WINSIZE must be an interger
+    Image = imread('Test_2.png');
+    
+    switch upper(Measure)
+        case 'LAPE'
+            LapOperator = fspecial('laplacian');
+            FM = imfilter(Image, LapOperator, 'replicate', 'conv');
+            FM = double(FM);
+            FM = FM.^2;
+            
+        case 'BREN'
+            [M, N] = size(Image);
+            DH = zeros(M, N);
+            DV = zeros(M, N);
+            DV(1:M-2,:) = Image(3:end,:)-Image(1:end-2,:);
+            DH(:,1:N-2) = Image(:,3:end)-Image(:,1:end-2);
+            FM = max(DH, DV);
+            FM = double(FM);
+            FM = FM.^2;
+            
+        case 'GRAE'
+            Ix = Image;
+            Iy = Image;
+            Iy(1:end-1,:) = diff(Image, 1, 1);
+            Ix(:,1:end-1) = diff(Image, 1, 2);
+            FM = double(Image);
+            FM = Ix.^2 + Iy.^2;
+    end   
+    
+    ResultDimX = size(Image, 1)/WINSIZE;
+    ResultDimY = size(Image, 2)/WINSIZE;
+    ResultImage = zeros(ResultDimX, ResultDimY);
+    
+    for i = 1:1:ResultDimX
+        for j = 1:1:ResultDimY
+            ResultImage(i, j) = mean2(imcrop(FM, [i*WINSIZE j*WINSIZE WINSIZE WINSIZE]));
+        end
+    end
+    
+    %ResultImage = ResultImage / max(max(ResultImage));
+    %BLevel = graythresh(ResultImage);
+    %ResultImage = im2bw(ResultImage, BLevel);
+    
+    %ResultImage = ResultImage + 1;
+    %ResultImage = log2(ResultImage);
+    ResultImage = uint16(fix(ResultImage / max(max(ResultImage)) * 65535));
+    ResultImage = histeq(ResultImage);
+    
+    ImageFDis = zeros(size(Image, 1), size(Image, 2));
+    for i = 1:1:ResultDimX
+        for j = 1:1:ResultDimY
+            for m = 1:WINSIZE
+                for n = 1:WINSIZE
+                    ImageFDis((i-1)*WINSIZE+m, (j-1)*WINSIZE+n) = ResultImage(i, j);
+                end
+            end
+        end
+    end
+    ImageFDis = uint16(ImageFDis);
+    
+    %figure, imshow(imcrop(Image, [300 300 499 499]));
+    %figure, imshow(imcrop(ImageFDis, [300 300 499 499]));
+    
+    figure, imshow(Image);
+    figure, imshow(ImageFDis);
+            
+end
--- Test.m
+++ Test.m
@@ -1,45 +1,75 @@
-function Test
-    WINSIZE = 3; %Image.Dimx / WINSIZE must be an interger
+% function Test
+    WINSIZE = 1; %Image.Dimx / WINSIZE must be an interger
+    
+% %     Image = imread('Test.png');
+% %     LapOperator = fspecial('laplacian');
+% %     FM = imfilter(Image, LapOperator, 'replicate', 'conv');
+% %     FM = double(FM);
+% %     FM = FM.^2;
+% %     
+% %     ResultDimX = size(Image, 1)/WINSIZE;
+% %     ResultDimY = size(Image, 2)/WINSIZE;
+% %     ResultImage = zeros(ResultDimX, ResultDimY);
+% %     
+% %     for i = 1:1:ResultDimX
+% %         for j = 1:1:ResultDimY
+% %             ResultImage(i, j) = mean2(imcrop(FM, [i*WINSIZE j*WINSIZE WINSIZE WINSIZE]));
+% %         end
+% %     end
+% %     
+% %     ResultImage = ResultImage / max(max(ResultImage));
+% %     BLevel = graythresh(ResultImage);
+% %     ResultImage = im2bw(ResultImage, BLevel);
+% %     
+% %     ImageFDis = zeros(size(Image, 1), size(Image, 2));
+% %     for i = 1:1:ResultDimX
+% %         for j = 1:1:ResultDimY
+% %             for m = 1:WINSIZE
+% %                 for n = 1:WINSIZE
+% %                     ImageFDis(i*WINSIZE+m, j*WINSIZE+n) = ResultImage(i, j);
+% %                 end
+% %             end
+% %         end
+% %     end
+% %     
+% %     figure, imshow(imcrop(Image, [500 500 299 299]));
+% %     figure, imshow(imcrop(ImageFDis, [500 500 299 299]));
+% %             
+    fclose all
+    clear all
+    close all
+    clc
+    
+    WINSIZE = 5;
     
     Image = imread('Test_2.png');
-    LapOperator = fspecial('laplacian');
-    FM = imfilter(Image, LapOperator, 'replicate', 'conv');
-    FM = double(FM);
-    FM = FM.^2;
+     
+    LAP = fspecial('laplacian');
+    FM = imfilter(double(Image), LAP, 'replicate', 'conv');
+    FM = (FM.^2);
+
+    figure
+    imagesc(Image)
+    colormap gray
+    axis off
+    title('Original image')
+
+    fun = @(block_struct) mean2(block_struct.data) * ones(size(block_struct.data));
+    I3 = blockproc(FM,[WINSIZE WINSIZE],fun);
+    norm_I3 = (I3 - min(I3(:))) / ( max(I3(:)) - min(I3(:)) );
+    norm_I3 = histeq(norm_I3);
     
-    ResultDimX = size(Image, 1)/WINSIZE;
-    ResultDimY = size(Image, 2)/WINSIZE;
-    ResultImage = zeros(ResultDimX, ResultDimY);
+    figure
+    imagesc(norm_I3)
+    colormap jet
+    axis off
+    title('LAPE with block mean')
     
-    for i = 1:1:ResultDimX
-        for j = 1:1:ResultDimY
-            ResultImage(i, j) = mean2(imcrop(FM, [i*WINSIZE j*WINSIZE WINSIZE WINSIZE]));
-        end
-    end
-    
-    %ResultImage = ResultImage / max(max(ResultImage));
-    %BLevel = graythresh(ResultImage);
-    %ResultImage = im2bw(ResultImage, BLevel);
-    
-    %ResultImage = ResultImage + 1;
-    %ResultImage = log2(ResultImage);
-    ResultImage = int16(fix(ResultImage / max(max(ResultImage)) * 65535));
-    ResultImage = histeq(ResultImage);
-    figure, imshow(ResultImage);
-    figure, imshow(Image);
-    
-    ImageFDis = zeros(size(Image, 1), size(Image, 2));
-    for i = 1:1:ResultDimX
-        for j = 1:1:ResultDimY
-            for m = 1:WINSIZE
-                for n = 1:WINSIZE
-                    ImageFDis(i*WINSIZE+m, j*WINSIZE+n) = ResultImage(i, j);
-                end
-            end
-        end
-    end
-    
-    figure, imshow(imcrop(Image, [500 500 299 299]));
-    figure, imshow(imcrop(ImageFDis, [500 500 299 299]));
-            
-end
+    BLevel = graythresh(norm_I3);
+    ResultImage = im2bw(norm_I3, BLevel);
+
+    figure
+    imagesc(ResultImage)
+    axis off
+    colormap jet
+    title('Binary separation with Otsu')
